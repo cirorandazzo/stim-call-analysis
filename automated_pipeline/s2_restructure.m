bf9e63adb7b738f1e0c243075fba19fbf7fc28b9
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-04-15 12:09:48 -0700

    fix bug of repeated trials in proc data

--- automated_pipeline/s2_restructure.m
+++ automated_pipeline/s2_restructure.m
@@ -21,7 +21,7 @@ end
 
 %%
 for cond=size(conditions,1):-1:1
-    data_i = ones([1 length(unproc_data)]);
+    data_i = ones([1 length(unproc_data)], 'logical');  % boolean index for data with this condition (ie, specific set of labels)
 
     for i=1:length(labels) % assign condition info to this struct
         param_name = labels{i};
@@ -36,7 +36,7 @@ for cond=size(conditions,1):-1:1
 
             proc_struct = [];
 
-            for struct_row=1:size(data_cut, 1)
+            for struct_row=1:size(data_cut, 1)  % row in unproc data; may contain >1 stim
                 x = data_cut(struct_row);
                 for data_row=1:size(data_cut(struct_row).breathing, 1)
                     
@@ -46,7 +46,7 @@ for cond=size(conditions,1):-1:1
 
                     int_struct = [];
 
-                    [int_struct.breathing, ...
+                   [int_struct.breathing, ...
                     int_struct.breathing_filt,...
                     int_struct.audio, ...
                     int_struct.latencies, ...
@@ -63,7 +63,7 @@ for cond=size(conditions,1):-1:1
                         radius, ...
                         insp_dur_max, ...
                         exp_delay, ...
-                        exp_dur_max);      
+                        exp_dur_max);
 
                     proc_struct = [proc_struct int_struct];
                 end
@@ -74,6 +74,8 @@ for cond=size(conditions,1):-1:1
             % proc_struct = arrayfun(@(x) getCallParamWrapper(x, deq_br, radius, insp_dur_max, ...
             %     exp_delay, exp_dur_max), data_cut);
 
+            % merges ALL rows (1 row/file) for this condition into 1 row in
+            % new struct
             proc_data(cond).breathing=cell2mat({proc_struct.breathing}');
             proc_data(cond).breathing_filt=cell2mat({proc_struct.breathing_filt}');
             proc_data(cond).audio=cell2mat({proc_struct.audio}');
