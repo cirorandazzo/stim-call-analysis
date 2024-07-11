dc15ec461d0f71da73439e90ad9fb5e8f2d0415a
Ciro Randazzo <ciro.randazzo@ucsf.edu>
2024-07-11 13:27:00 -0700

    reorganize dm/pam stim parameter files

--- parameters/dm_pam/pam_stim/bu26bu73.m
+++ parameters/dm_pam/pam_stim/bu26bu73.m
@@ -19,6 +19,9 @@ p.breath_seg.min_duration_fr = 10 * p.fs / 1000;
 p.breath_seg.exp_thresh = 0.01;
 p.breath_seg.insp_thresh = -0.03;
 
-
+%--call segmentation options
+p.call_seg.q = 1.8;  % threshold = p.call_seg.q*MEDIAN
+p.call_seg.min_interval_ms = 5;  % ms; minimum time between 2 notes to be considered separate notes (else merged)
+p.call_seg.min_duration_ms = 7;  % ms; minimum duration of note to be considered (else ignored)1
 
 
