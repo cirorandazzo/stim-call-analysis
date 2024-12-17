e4a94ba0d474318da03ddbe6cee632b7545417c1
Ciro Randazzo <cirorandazzo13@gmail.com>
2024-12-17 11:29:42 -0800

    update pk70pu50 pam stim call window

--- parameters/dm_pam/pam_stim/pk70pu50.m
+++ parameters/dm_pam/pam_stim/pk70pu50.m
@@ -3,7 +3,7 @@
 % saving/loading parameters
 
 %--files
-p.files.raw_data = 'F:\ziggy\stim_data-20240604\pam\pk70pu50';
+p.files.raw_data = '/Volumes/PlasticBag/ziggy/stim_data-20240604/pam/pk70pu50';
 
 p.files.bird_name = 'pk70pu50';
 p.files.group = 'pam';
@@ -16,6 +16,9 @@ p = default_params(p);
 p.call_seg.q = 3;  % threshold = p.call_seg.q*MEDIAN
 p.call_seg.min_interval_ms = 30;  % ms; minimum time between 2 notes to be considered separate notes (else merged)
 p.call_seg.min_duration_ms = 10;  % ms; minimum duration of note to be considered (else ignored)
+
+% reject 1 trial with noise at 195ms (usual range: 10-350ms)
+p.call_seg.post_stim_call_window_ms = [10 190];
 
 %--breath segmentation
 p.breath_seg.min_duration_fr = 10 * p.fs / 1000;
